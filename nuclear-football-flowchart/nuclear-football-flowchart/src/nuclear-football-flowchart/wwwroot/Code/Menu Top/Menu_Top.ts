ed9eb7bee9a283eb4be4d9d5f05285dd1557ff4c
Aaron Campf <aaroncampf@gmail.com>
2017-01-11 13:27:11 -0800

    Commit

---
+++ nuclear-football-flowchart/nuclear-football-flowchart/src/nuclear-football-flowchart/wwwroot/Code/Menu Top/Menu_Top.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+
+/**
+Menu for top of each web page
+*/
+
+@Component({
+	selector: 'Menu-Top',
+	template: 
+`
+<h1>Menu</h1>
+
+`
+})
+export class Menu_Top { }
---
+++ nuclear-football-flowchart/nuclear-football-flowchart/src/nuclear-football-flowchart/wwwroot/app/app.module.ts
@@ -0,0 +1,11 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { Menu_Top }   from './Menu_Top';
+
+@NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [AppComponent, Menu_Top ],
+    bootstrap: [AppComponent, Menu_Top]
+})
+export class AppModule { }
